9b7cc0a7192fc77b50a03e9c4e78370ff00d24cd
waleedrimawi1 <waleedrimawi1@gmail.com>
2025-08-02 21:27:25 +0300

    fix checkbox and add TodoItemComponent

--- src/app/todo-item/todo-item.ts
+++ src/app/todo-item/todo-item.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Task {
+  text: string;
+  isChecked: boolean;
+}
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.css'
 })
 export class TodoItem {
+  item = input.required<Task>();
+  index = input.required<number>();
+  delete = output<number>();
 
+  onDelete() {
+    this.delete.emit(this.index());
+  }
 }
--- src/app/todo-list/todo-list.ts
+++ src/app/todo-list/todo-list.ts
@@ -1,30 +1,30 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { TodoItem, Task } from '../todo-item/todo-item';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [FormsModule,CommonModule ],
+  imports: [FormsModule, CommonModule, TodoItem],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css'
 })
 export class TodoList {
   inputValue = ''
-  items:string[] = []
-  isChecked = false
+  items: Task[] = []
 
   addItem() {
     if(this.inputValue){
-      this.items.push(this.inputValue)
+      this.items.push({
+        text: this.inputValue,
+        isChecked: false
+      })
       this.inputValue = ''
     }
   }
 
-  deleteItem(index:number){
-    this.items.splice(index,1)
+  deleteItem(index: number){
+    this.items.splice(index, 1)
   }
 }
-
-
