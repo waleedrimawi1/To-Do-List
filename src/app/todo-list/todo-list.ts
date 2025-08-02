d9a51501ca0bccaab0a24efef777af35c0af32e0
waleedrimawi1 <waleedrimawi1@gmail.com>
2025-08-02 20:33:10 +0300

    add functionality to CheckBox

--- src/app/todo-list/todo-list.ts
+++ src/app/todo-list/todo-list.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-todo-list',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule,CommonModule ],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css'
 })
 export class TodoList {
   inputValue = ''
   items:string[] = []
-  
+  isChecked = false
+
   addItem() {
-    if(this.items){
+    if(this.inputValue){
       this.items.push(this.inputValue)
       this.inputValue = ''
     }
